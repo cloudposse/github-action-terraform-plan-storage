1b092532fb7cb4f423ef77368ead960447bebf27
Matt Calhoun <matt@cloudposse.com>
2023-01-23 20:26:58 -0500

    fix dynamo query

--- src/modules/terraformPlan/mapper/dynamoMapper.ts
+++ src/modules/terraformPlan/mapper/dynamoMapper.ts
@@ -10,7 +10,20 @@ import { TerraformPlanStack } from "../domain/TerraformPlanStack";
 export class TerraformPlanDynamoDBMapper extends Mapper<TerraformPlan> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public toDomain(raw: any): TerraformPlan {
-    throw new Error(JSON.stringify(raw));
+    //    {
+    //   "repoName": "demo-full-workflow",
+    //   "commitSHA": "62f7f2f32bcc970970b277dd54c11cf5109f90c4",
+    //   "pr": 14,
+    //   "component": "dummy",
+    //   "contentsHash": "aa509448d341b4012e3c89278c30506793bebacf1c51aa74c103bda853319df8",
+    //   "tainted": false,
+    //   "branch": "feature/test16",
+    //   "stack": "ue1-dev",
+    //   "createdAt": "2023-01-24T01:17:05.015Z",
+    //   "id": "0359de0a-d47c-41a3-b89b-933c9eee7620",
+    //   "repoOwner": "cloudposse-sandbox"
+    // }
+
     const planOrError = TerraformPlan.create(
       {
         branch: TerraformPlanBranch.create({ value: raw.branch }).getValue(),
@@ -21,7 +34,7 @@ export class TerraformPlanDynamoDBMapper extends Mapper<TerraformPlan> {
           value: raw.component,
         }).getValue(),
         pr: TerraformPlanPR.create({
-          value: raw.pr.N,
+          value: raw.pr,
         }).getValue(),
         stack: TerraformPlanStack.create({
           value: raw.stack,
