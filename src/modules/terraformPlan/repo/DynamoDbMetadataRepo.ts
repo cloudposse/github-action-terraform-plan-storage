bab6936eea3b9edcfd2c8b83dc2bef01d23ddcbc
Matt Calhoun <matt@cloudposse.com>
2023-01-23 19:30:05 -0500

    fix dynamo query

--- src/modules/terraformPlan/repo/DynamoDbMetadataRepo.ts
+++ src/modules/terraformPlan/repo/DynamoDbMetadataRepo.ts
@@ -85,16 +85,19 @@ export class DynamoDBMetadataRepo implements IMetadataRepository {
     const command = new ScanCommand(params);
     const response = await this.dynamo.send(command);
 
-    if (!response.Items || response.Items.length === 0) {
-      throw new RepositoryErrors.PlanNotFoundError(
-        component,
-        stack,
-        undefined,
-        pr
-      );
-    }
+    throw new Error(JSON.stringify(response.Items, null, 2));
+    return {} as TerraformPlan;
 
-    return this.mapper.toDomain(response.Items[0]);
+    // if (!response.Items || response.Items.length === 0) {
+    //   throw new RepositoryErrors.PlanNotFoundError(
+    //     component,
+    //     stack,
+    //     undefined,
+    //     pr
+    //   );
+    // }
+
+    // return this.mapper.toDomain(response.Items[0]);
   }
 
   public async save(plan: TerraformPlan): Promise<void> {
