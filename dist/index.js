a09c0df0a4390744ea6c7b93541ade5f4b7d9b96
mcalhoun <mcalhoun@users.noreply.github.com>
2023-06-16 03:24:44 +0000

    Update contents of the dist directory

--- dist/index.js
+++ dist/index.js
@@ -62316,6 +62316,29 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -62329,6 +62352,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.GetTerraformPlanUseCase = void 0;
 const fs_1 = __nccwpck_require__(57147);
 const stream_1 = __nccwpck_require__(12781);
+const core = __importStar(__nccwpck_require__(42186));
 const crypto_1 = __nccwpck_require__(65788);
 const infrastructure_1 = __nccwpck_require__(73950);
 const readable_1 = __nccwpck_require__(24748);
@@ -62372,6 +62396,9 @@ class GetTerraformPlanUseCase {
                 }
                 const contents = yield (0, readable_1.stringFromReadable)(plan);
                 const hash = yield (0, crypto_1.calculateHash)(contents);
+                core.info(`Hash of plan: ${hash}`);
+                core.info(`Hash in metadata: ${metadata.contentsHash}`);
+                core.info(`Plan Contents: ${contents}`);
                 if (metadata.contentsHash != hash) {
                     return (0, infrastructure_1.left)(new errors_1.GetTerraformPlanErrors.ContentsHashMismatch((_b = (_a = metadata.contentsHash) === null || _a === void 0 ? void 0 : _a.toString()) !== null && _b !== void 0 ? _b : "", hash));
                 }
