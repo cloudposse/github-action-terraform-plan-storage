3cc3ef73d99792d43d293da70496495c3d2a3300
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-08-28 14:26:01 +0300

    update

--- src/modules/terraformPlan/repo/DynamoDbMetadataRepo.ts
+++ src/modules/terraformPlan/repo/DynamoDbMetadataRepo.ts
@@ -30,6 +30,8 @@ export class DynamoDBMetadataRepo implements IMetadataRepository {
     stack: string,
     commitSHA: string
   ): Promise<TerraformPlan> {
+    console.log(`owner: ${owner}, repo: ${repo}, component: ${component}, stack: ${stack}, commitSHA: ${commitSHA}`);
+
     const params: ScanCommandInput = {
       TableName: this.tableName,
       FilterExpression:
@@ -56,7 +58,11 @@ export class DynamoDBMetadataRepo implements IMetadataRepository {
     const response = await this.dynamo.send(command);
 
     if (!response.Items || response.Items.length === 0) {
+      console.log(`plan not found for ${owner}/${repo}/${component}/${stack}/${commitSHA}`);
       throw new RepositoryErrors.PlanNotFoundError(component, stack, commitSHA);
+    } else {
+      console.log(`plan found for ${owner}/${repo}/${component}/${stack}/${commitSHA}`);
+      console.log(`${response.Items.length} items returned`);
     }
 
     const itemsReturned = response.Items.length;
